a90d2ca6b97c5b6a6c46907d9c0805f747b8f5b2
rusthigh <rusthigh@proton.me>
2022-06-01 22:41:43 +0000

    version 0.9

--- data/apada2seg_dataset.py
+++ data/apada2seg_dataset.py
@@ -1,3 +1,22 @@
 import os.path
 import torchvision.transforms as transforms
-from data.base_dataset import BaseDataset
+from data.base_dataset import BaseDataset, get_transform
+from PIL import Image
+import torch
+import random
+import data.random_pair as random_pair
+import pdb
+
+
+class TrainDataset(BaseDataset):
+    def initialize(self, opt):
+        self.opt = opt
+
+        self.dir_A = opt.raw_A_dir
+        self.dir_B = opt.raw_B_dir
+        self.dir_Seg = opt.raw_A_seg_dir
+
+        self.A_paths = opt.imglist_A
+        self.B_paths = opt.imglist_B
+
+        self.A_size = len(self
