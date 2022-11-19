a3a2c2e8c39fa46aeaa6f04cdc2ffcadefb15d57
rusthigh <rusthigh@proton.me>
2022-11-19 14:52:47 +0000

    f2fs: clean up f2fs_balance_fs

--- data/apada2seg_test_dataset.py
+++ data/apada2seg_test_dataset.py
@@ -8,4 +8,20 @@ import data.random_pair as random_pair
 
 
 class TestDataset(BaseDataset):
-    def initial
+    def initialize(self, opt):
+        self.opt = opt
+
+        self.dir_B = opt.test_B_dir
+
+        self.B_filenames = opt.imglist_testB
+        self.B_size = len(self.B_filenames)
+
+        if not self.opt.isTrain:
+            self.skipcrop = True
+            self.skiprotate = True
+        else:
+            self.skipcrop = False
+            self.skiprotate = False
+
+        if self.skipcrop:
+            osize = [opt.fineSize, opt.fineS
