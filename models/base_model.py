778aae6355dc2c67f96caffcaafc8072b64bc6bc
rusthigh <rusthigh@proton.me>
2023-04-02 10:03:03 +0000

    - Initial Commit

--- models/base_model.py
+++ models/base_model.py
@@ -9,4 +9,21 @@ class BaseModel():
     def initialize(self, opt):
         self.opt = opt
         self.gpu_ids = opt.gpu_ids
+        self.isTrain = opt.isTrain
+        self.Tensor = torch.cuda.FloatTensor if self.gpu_ids else torch.Tensor
+        self.save_dir = os.path.join(opt.checkpoints_dir, opt.name)
+
+    def set_input(self, input):
+        self.input = input
+
+    def forward(self):
+        pass
+
+    # used in test time, no backprop
+    def test(self):
+        pass
+
+    def get_image_paths(self):
+        pass
+
     
