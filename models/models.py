ad64ec58e277e4de6db4ebe4463a569b916254f7
rusthigh <rusthigh@proton.me>
2023-04-17 21:30:18 +0000

    compiler-gcc: disable -ftracer for __noclone functions

--- models/models.py
+++ models/models.py
@@ -5,4 +5,15 @@ def create_model(opt):
     # our anatomy-preserving adaptation segmentation
     if opt.model == 'apada2seg_model_train':
         assert(opt.dataset_mode == 'apada2seg_train')
-        from .apada2seg_model import 
+        from .apada2seg_model import APADA2SEGModel
+        model = APADA2SEGModel()
+    elif opt.model == 'apada2seg_model_test':
+        assert(opt.dataset_mode == 'apada2seg_test')
+        from .apada2seg_model import APADA2SEGModel_TEST
+        model = APADA2SEGModel_TEST()
+
+    else:
+        raise ValueError("Model [%s] not recognized." % opt.model)
+
+    model.initialize(opt)
+    print("model [%s] was created" % (model.name(
