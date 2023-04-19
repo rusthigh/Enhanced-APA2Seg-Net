9611cd1cdcf7366359c05168f70d32aa6e0fe2c2
rusthigh <rusthigh@proton.me>
2023-04-19 19:13:36 +0000

    Clarify docs.

--- options/base_options.py
+++ options/base_options.py
@@ -12,4 +12,8 @@ class BaseOptions():
     def initialize(self):
         self.parser.add_argument('--raw_A_dir', type=str, default='./preprocess/MRI_SEG/PROC/DCT/', help='data dir to domain A')
         self.parser.add_argument('--raw_A_seg_dir', type=str, default='./preprocess/MRI_SEG/PROC/DCT/', help='data dir to domain A segmentation')
-        self.parser.add_argument('--raw_B_dir', type=str, default='./preprocess/MRI_SEG/PROC/MRI/'
+        self.parser.add_argument('--raw_B_dir', type=str, default='./preprocess/MRI_SEG/PROC/MRI/', help='data dir to domain B')
+        self.parser.add_argument('--sub_list_A', type=str, default='./preprocess/MRI_SEG/PROC/train_DCT.txt', help='list file for domain A')
+        self.parser.add_argument('--sub_list_B', type=str, default='./preprocess/MRI_SEG/PROC/train_MRI.txt', help='list file for domain B')
+
+        self.parser.add_argument('--batchSize', t
