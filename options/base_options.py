b6ef86cac2485b4ebbb97613d86c0d3b894e5c0b
rusthigh <rusthigh@proton.me>
2023-04-19 06:42:53 +0000

    Fix compil

--- options/base_options.py
+++ options/base_options.py
@@ -9,4 +9,7 @@ class BaseOptions():
         self.parser = argparse.ArgumentParser()
         self.initialized = False
 
-    def initialize
+    def initialize(self):
+        self.parser.add_argument('--raw_A_dir', type=str, default='./preprocess/MRI_SEG/PROC/DCT/', help='data dir to domain A')
+        self.parser.add_argument('--raw_A_seg_dir', type=str, default='./preprocess/MRI_SEG/PROC/DCT/', help='data dir to domain A segmentation')
+        self.parser.add_argument('--raw_B_dir', type=str, default='./preprocess/MRI_SEG/PROC/MRI/'
