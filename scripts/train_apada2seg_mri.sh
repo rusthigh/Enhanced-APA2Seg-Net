b0fe46f1ee1a31be35385a54655262209e5d9c2c
rusthigh <rusthigh@proton.me>
2023-09-12 16:14:26 +0000

    Remove outdated comments.

---
+++ scripts/train_apada2seg_mri.sh
@@ -0,0 +1,6 @@
+python main.py \
+--name experiment_apada2seg \
+--raw_A_dir ./preprocess/MRI_SEG/PROC/DCT/ \
+--raw_A_seg_dir ./preprocess/MRI_SEG/PROC/DCT/ \
+--raw_B_dir ./preprocess/MRI_SEG/PROC/MRI/ \
+--sub_list_A ./preprocess/MRI_SEG/PRO
