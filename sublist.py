831381c9875658091dd89f7812b907d94a10e532
rusthigh <rusthigh@proton.me>
2023-09-17 14:43:28 +0000

    chg: [misp-objects] updated to the latest version

--- sublist.py
+++ sublist.py
@@ -16,4 +16,16 @@ def dir2list(path, sub_list_file):
         fp = open(sub_list_file, 'r')
         sublines = fp.readlines()
         sub_names = []
-        for s
+        for subline in sublines:
+            sub_info = subline.replace('\n', '')
+            sub_names.append(sub_info)
+        fp.close()
+        return sub_names
+    else:
+        fp = open(sub_list_file, 'w')
+        img_root_dir = os.path.join(path)
+        subs = os.listdir(img_root_dir)
+        subs.sort()
+        for sub in subs:
+            sub_dir = os.path.join(img_root_dir,sub)
+            views = os.listdir(sub
