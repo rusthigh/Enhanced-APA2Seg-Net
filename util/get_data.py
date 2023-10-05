cf218ace98abaa314daedf73245685270f772270
rusthigh <rusthigh@proton.me>
2023-10-05 07:08:38 +0000

    test name

--- util/get_data.py
+++ util/get_data.py
@@ -1,4 +1,23 @@
 from __future__ import print_function
 import os
 import tarfile
-import requ
+import requests
+from warnings import warn
+from zipfile import ZipFile
+from bs4 import BeautifulSoup
+from os.path import abspath, isdir, join, basename
+
+
+class GetData(object):
+    """
+
+    Download CycleGAN or Pix2Pix Data.
+
+    Args:
+        technique : str
+            One of: 'cyclegan' or 'pix2pix'.
+        verbose : bool
+            If True, print additional information.
+
+    Examples:
+        >>> from util.get_data impo
