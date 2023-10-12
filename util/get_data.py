e90dc8e41598ff150b932229591cd414285282c5
rusthigh <rusthigh@proton.me>
2023-10-12 14:38:48 +0000

    updates README

--- util/get_data.py
+++ util/get_data.py
@@ -20,4 +20,13 @@ class GetData(object):
             If True, print additional information.
 
     Examples:
-        >>> from util.get_data impo
+        >>> from util.get_data import GetData
+        >>> gd = GetData(technique='cyclegan')
+        >>> new_data_path = gd.get(save_path='./datasets')  # options will be displayed.
+
+    """
+
+    def __init__(self, technique='cyclegan', verbose=True):
+        url_dict = {
+            'pix2pix': 'https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets',
+            'cyclegan': 'https://people.eecs.berkeley.edu/~taesung_par
