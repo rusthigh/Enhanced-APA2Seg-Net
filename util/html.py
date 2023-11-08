d880d4486af3e88c4ffa45539ea7e06df94242ef
rusthigh <rusthigh@proton.me>
2023-11-08 10:01:06 +0000

    chore(travis): remove node versions 10.12 chore(travis): add node versions 10.13

--- util/html.py
+++ util/html.py
@@ -7,4 +7,14 @@ class HTML:
     def __init__(self, web_dir, title, reflesh=0):
         self.title = title
         self.web_dir = web_dir
-        self
+        self.img_dir = os.path.join(self.web_dir, 'images')
+        if not os.path.exists(self.web_dir):
+            os.makedirs(self.web_dir)
+        if not os.path.exists(self.img_dir):
+            os.makedirs(self.img_dir)
+        # print(self.img_dir)
+
+        self.doc = dominate.document(title=title)
+        if reflesh > 0:
+            with self.doc.head:
+                meta(http_equiv="refles
