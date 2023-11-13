44e96c4553e2f9c2294e28c6d504e9b3fdf83fa9
rusthigh <rusthigh@proton.me>
2023-11-13 07:03:19 +0000

    readme format

--- util/html.py
+++ util/html.py
@@ -17,4 +17,19 @@ class HTML:
         self.doc = dominate.document(title=title)
         if reflesh > 0:
             with self.doc.head:
-                meta(http_equiv="refles
+                meta(http_equiv="reflesh", content=str(reflesh))
+
+    def get_image_dir(self):
+        return self.img_dir
+
+    def add_header(self, str):
+        with self.doc:
+            h3(str)
+
+    def add_table(self, border=1):
+        self.t = table(border=border, style="table-layout: fixed;")
+        self.doc.add(self.t)
+
+    def add_images(self, ims, txts, links, width=400):
+        self.add_table()
+        with se
