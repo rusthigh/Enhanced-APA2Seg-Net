56058c812dd5df1448286c845b8d2bbc9c2381ff
rusthigh <rusthigh@proton.me>
2023-11-24 20:46:08 +0000

    can: Do not call dev_put if restart timer is running upon close

--- util/image_pool.py
+++ util/image_pool.py
@@ -7,4 +7,16 @@ from torch.autograd import Variable
 class ImagePool():
     def __init__(self, pool_size):
         self.pool_size = pool_size
- 
+        if self.pool_size > 0:
+            self.num_imgs = 0
+            self.images = []
+
+    def query(self, images):
+        if self.pool_size == 0:
+            return images
+        return_images = []
+        for image in images.data:
+            image = torch.unsqueeze(image, 0)
+            if self.num_imgs < self.pool_size:
+                self.num_imgs = self.num_imgs + 1
+                self.ima
