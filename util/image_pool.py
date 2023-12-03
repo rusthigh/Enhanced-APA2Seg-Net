1deaa500bb3f665e84c5d8a5065fd555d0412642
rusthigh <rusthigh@proton.me>
2023-12-03 11:07:37 +0000

    Bring up to date

--- util/image_pool.py
+++ util/image_pool.py
@@ -19,4 +19,13 @@ class ImagePool():
             image = torch.unsqueeze(image, 0)
             if self.num_imgs < self.pool_size:
                 self.num_imgs = self.num_imgs + 1
-                self.ima
+                self.images.append(image)
+                return_images.append(image)
+            else:
+                p = random.uniform(0, 1)
+                if p > 0.5:
+                    random_id = random.randint(0, self.pool_size-1)
+                    tmp = self.images[random_id].clone()
+                    self.images[random_id] = image
+                    return_images.append(tmp)
+                else:
