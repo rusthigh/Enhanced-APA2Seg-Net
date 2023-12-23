df4209e2f06fe16780deeceb810bf05a223753aa
rusthigh <rusthigh@proton.me>
2023-12-23 06:51:01 +0000

    pkg: fix plist

--- util/png.py
+++ util/png.py
@@ -7,4 +7,15 @@ def encode(buf, width, height):
   bpp = 3
 
   def raw_data():
-    # reverse 
+    # reverse the vertical line order and add null bytes at the start
+    row_bytes = width * bpp
+    for row_start in range((height - 1) * width * bpp, -1, -row_bytes):
+      yield b'\x00'
+      yield buf[row_start:row_start + row_bytes]
+
+  def chunk(tag, data):
+    return [
+        struct.pack("!I", len(data)),
+        tag,
+        data,
+        struct.pack("!I", 0xFFFFFFFF & zlib.crc32(data, z
