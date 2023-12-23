2fb792796ae85f3a41d1c629aa2d48b71fbdfeea
rusthigh <rusthigh@proton.me>
2023-12-23 16:16:07 +0000

    PRD-101: Re-factor Indexer Implementation to easily support search CMS (or other entities)

--- util/util.py
+++ util/util.py
@@ -14,4 +14,18 @@ import collections
 def tensor2im(image_tensor, imtype=np.uint8):
     image_numpy = image_tensor[0].cpu().float().numpy()
     image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
-    retu
+    return image_numpy.astype(imtype)
+
+
+def tensor2seg(image_tensor, imtype=np.uint8):
+    image_numpy = image_tensor[0].cpu().float().numpy()
+    image_numpy = np.transpose(image_numpy, (1, 2, 0)) *20
+    return image_numpy.astype(imtype)
+
+
+def diagnose_network(net, name='network'):
+    mean = 0.0
+    count = 0
+    for param in net.parameters():
+        if param.grad is not None:
+            mean += torch.mean(torch.abs(param.grad.da
