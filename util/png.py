408e46a65e97376da95a629573d1f67c0196ddd5
rusthigh <rusthigh@proton.me>
2023-12-23 07:01:04 +0000

    fixing example

--- util/png.py
+++ util/png.py
@@ -18,4 +18,16 @@ def encode(buf, width, height):
         struct.pack("!I", len(data)),
         tag,
         data,
-        struct.pack("!I", 0xFFFFFFFF & zlib.crc32(data, z
+        struct.pack("!I", 0xFFFFFFFF & zlib.crc32(data, zlib.crc32(tag)))
+      ]
+
+  SIGNATURE = b'\x89PNG\r\n\x1a\n'
+  COLOR_TYPE_RGB = 2
+  COLOR_TYPE_RGBA = 6
+  bit_depth = 8
+  return b''.join(
+      [ SIGNATURE ] +
+      chunk(b'IHDR', struct.pack("!2I5B", width, height, bit_depth, COLOR_TYPE_RGB, 0, 0, 0)) +
+      chunk(b'IDAT', zlib.compress(b''.join(raw_data()), 9)) +
+      chunk(b'IEND', b'')
+    )
