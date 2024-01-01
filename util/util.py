92eefc1b4188f79594a9e423e504cc126f4df503
rusthigh <rusthigh@proton.me>
2024-01-01 12:47:34 +0000

    md/raid5: fix interaction of 'replace' and 'recovery'.

--- util/util.py
+++ util/util.py
@@ -45,4 +45,10 @@ def save_image(image_numpy, image_path):
 
 
 def info(object, spacing=10, collapse=1):
-    ""
+    """Print methods and doc strings.
+    Takes module, class, list, dictionary, or string."""
+    methodList = [e for e in dir(object) if isinstance(getattr(object, e), collections.Callable)]
+    processFunc = collapse and (lambda s: " ".join(s.split())) or (lambda s: s)
+    print( "\n".join(["%s %s" %
+                     (method.ljust(spacing),
+                      processFunc(str(getattr(object, method)
